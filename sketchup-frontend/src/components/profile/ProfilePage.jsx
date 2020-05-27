917c890dbc7a7947bdefc0e793a94ac98f33b9fd
AidanMcB <aidankmcbride@gmail.com>
2020-05-26 19:33:03 -0500

    updated login show image pages

--- sketchup-frontend/src/components/profile/ProfilePage.jsx
+++ sketchup-frontend/src/components/profile/ProfilePage.jsx
@@ -14,8 +14,29 @@ export function ProfilePage(props){
         console.log(currentUser)
     }
     return (
-            <div>
-                <h1>Logged in As:</h1>
+        //finish profile page and active storage in rails
+        <div class="ui card">
+        <div class="image">
+          <img src="/images/avatar2/large/kristy.png" />
+        </div>
+        <div class="content">
+          <a class="header">{currentUser.username}</a>
+          <div class="meta">
+            <span class="date">Joined in 2013</span>
+          </div>
+          <div class="description">
+            Kristy is an art director living in New York.
+          </div>
+        </div>
+        <div class="extra content">
+          <a>
+            <i class="user icon"></i>
+            22 Friends
+          </a>
+        </div>
+      </div>
+            /* <div>
+                <h1>Welcome</h1>
                 <h1>{currentUser.username }</h1>
                 <h2>Image Gallery:</h2>
                 {(currentUser.images) ? 
@@ -24,7 +45,7 @@ export function ProfilePage(props){
                 )
                 :
                 null}
-            </div>
+            </div> */
         )
     
 }
