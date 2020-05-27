f44effa6eaae604d0b0cafe423131033c76a995c
AidanMcB <aidankmcbride@gmail.com>
2020-05-27 11:57:54 -0500

    images resized waiting to figure out non-refresh profile page

--- sketchup-frontend/src/components/ImageIndex/ImageCard.jsx
+++ sketchup-frontend/src/components/ImageIndex/ImageCard.jsx
@@ -22,6 +22,7 @@ export function ImageCard(props){
                     <img 
                     alt='' 
                     src={image.url}
+                    class="ui fluid image"
                     onClick={()=> history.push(`/show/${image.id}`)}
                     />  
                 </div>
--- sketchup-frontend/src/components/profile/ProfilePage.jsx
+++ sketchup-frontend/src/components/profile/ProfilePage.jsx
@@ -5,7 +5,7 @@ import { useHistory } from 'react-router'
 import { UploadButton } from './ProfileButtons/UploadButton'
 
 export function ProfilePage(props){
-
+  //update user and his / her images here so profile page refreshes
     let history = useHistory()
    
     let currentUser = props.currentUser
@@ -15,31 +15,7 @@ export function ProfilePage(props){
     }
     console.log(currentUser.images)
     return (
-
-        //finish profile page and active storage in rails
-      //   <div class="ui card">
-      //   <UploadButton />  
-      //   <div class="image">
-      //     <img src="/images/avatar2/large/kristy.png" />
-      //   </div>
-      //   <div class="content">
-      //     <a class="header">{currentUser.username}</a>
-      //     <div class="meta">
-      //       <span class="date">Joined in 2013</span>
-      //     </div>
-      //     <div class="description">
-      //       Kristy is an art director living in New York.
-      //     </div>
-      //   </div>
-      //   <div class="extra content">
-      //     <a>
-      //       <i class="user icon"></i>
-      //       22 Friends
-        
-      //     </a>
-      //   </div>
-      // </div>
-            <div>
+         <div>
             <UploadButton />
                 <h1>Welcome</h1>
                 <h1>{currentUser.username }</h1>
@@ -47,7 +23,9 @@ export function ProfilePage(props){
                 <img src={currentUser.images[5]}/>
                 {(currentUser.images != []) ? 
                 currentUser.images.map( image => 
-                  <img onClick={() => history.push(`/show/${image.id}`)} src={image.url}/>  
+                <div class="ui card" class="ui segment">
+                  <img onClick={() => history.push(`/show/${image.id}`)} src={image.url} class="ui fluid image" class="ui medium centered image" />  
+                </div>
                 )
                 :
                 null}
